5f7eb095d60fc8bbd399689753b822a8d7e6e608
Ivan De Marino <idemarino@newrelic.com>
2016-08-26 12:46:26 +0100

    Adding tests against HTTP and HTTPS

---
+++ test/http.js
@@ -0,0 +1,71 @@
+const http = require('http');
+const url = require('url');
+const assert = require('assert');
+
+const CaronteProxy = require('../');
+
+const PROXY_HOST = 'localhost';
+const PROXY_PORT = 9999;
+
+var httpProxyAgent = new (require('http-proxy-agent'))('http://' + PROXY_HOST + ':' + PROXY_PORT);
+
+var proxyRequestCounter = 0;
+var proxy = CaronteProxy(function (req, res) {
+  ++proxyRequestCounter;
+});
+
+describe('Caronte Proxy - HTTP - No Auth', function () {
+  before(function startProxy(done) {
+    proxy.on('listening', done);
+    proxy.listen(PROXY_PORT);
+  });
+
+  it('should let HTTP traffic through', function (done) {
+    var reqOpts = url.parse('http://httpbin.org/headers');
+    reqOpts.agent = httpProxyAgent;
+    reqOpts.headers = {
+      'Test-Header-1': 'Test-Value-1',
+      'Test-Header-2': 'Test-Value-2'
+    };
+    var resBody = [];
+
+    http.request(reqOpts, function (res) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(proxyRequestCounter, 1);
+
+      res
+        .on('data', function (chunk) {
+          resBody.push(chunk);
+        })
+        .on('end', function () {
+          resBody = Buffer.concat(resBody).toString();
+
+          reqHeaders = JSON.parse(resBody).headers;
+          assert(!!reqHeaders['Test-Header-1']);
+          assert(!!reqHeaders['Test-Header-2']);
+          assert.strictEqual(reqHeaders['Test-Header-1'], 'Test-Value-1');
+          assert.strictEqual(reqHeaders['Test-Header-2'], 'Test-Value-2');
+
+          done();
+        });
+    }).end();
+  });
+
+  it('should let HTTP redirects through', function (done) {
+    var reqOpts = url.parse('http://httpbin.org/redirect-to?url=http://httpbin.org/headers');
+    reqOpts.agent = httpProxyAgent;
+
+    http.request(reqOpts, function (res) {
+      assert.equal(res.statusCode, 302);
+      assert.equal(proxyRequestCounter, 2);
+      assert(!!res.headers['location']);
+      assert.strictEqual(res.headers['location'], 'http://httpbin.org/headers');
+      done();
+    }).end();
+  });
+
+  after(function stopProxy(done) {
+    proxy.on('close', done);
+    proxy.close();
+  });
+});
---
+++ test/https.js
@@ -0,0 +1,86 @@
+const https = require('https');
+const url = require('url');
+const assert = require('assert');
+
+const CaronteProxy = require('../');
+
+const PROXY_HOST = 'localhost';
+const PROXY_PORT = 9999;
+
+var httpsProxyAgent = new (require('https-proxy-agent'))('http://' + PROXY_HOST + ':' + PROXY_PORT);
+
+var proxyRequestCounter = 0;
+var proxy = CaronteProxy(function (req, res) {
+  ++proxyRequestCounter;
+});
+
+describe('Caronte Proxy - HTTPS (self signed) - No Auth', function () {
+  before(function startProxy(done) {
+    proxy.on('listening', done);
+    proxy.listen(PROXY_PORT);
+  });
+
+  it('should let HTTPS traffic through', function (done) {
+    var reqOpts = url.parse('https://httpbin.org/headers');
+    reqOpts.agent = httpsProxyAgent;
+    reqOpts.headers = {
+      'Test-Header-1': 'Test-Value-1',
+      'Test-Header-2': 'Test-Value-2'
+    };
+    reqOpts.rejectUnauthorized = false;
+    var resBody = [];
+
+    https.request(reqOpts, function (res) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(proxyRequestCounter, 1);
+
+      res
+        .on('data', function (chunk) {
+          resBody.push(chunk);
+        })
+        .on('end', function () {
+          resBody = Buffer.concat(resBody).toString();
+
+          reqHeaders = JSON.parse(resBody).headers;
+          assert(!!reqHeaders['Test-Header-1']);
+          assert(!!reqHeaders['Test-Header-2']);
+          assert.strictEqual(reqHeaders['Test-Header-1'], 'Test-Value-1');
+          assert.strictEqual(reqHeaders['Test-Header-2'], 'Test-Value-2');
+
+          done();
+        });
+    }).end();
+  });
+
+  it('should let HTTPS redirects through', function (done) {
+    var reqOpts = url.parse('https://httpbin.org/redirect-to?url=http://httpbin.org/headers');
+    reqOpts.agent = httpsProxyAgent;
+    reqOpts.rejectUnauthorized = false;
+
+    https.request(reqOpts, function (res) {
+      assert.equal(res.statusCode, 302);
+      assert.equal(proxyRequestCounter, 2);
+      assert(!!res.headers['location']);
+      assert.strictEqual(res.headers['location'], 'http://httpbin.org/headers');
+      done();
+    }).end();
+  });
+
+  it('should throw on HTTPS request when Self-Signed certificate is unacceptable', function (done) {
+    var reqOpts = url.parse('https://httpbin.org/');
+    reqOpts.agent = httpsProxyAgent;
+
+    https.request(reqOpts, function (res) {
+      // THIS SHOULD NEVER HAPPEN
+    }).on('error', function(err) {
+      assert.equal(err.message, 'self signed certificate');
+      assert.equal(err.code, 'DEPTH_ZERO_SELF_SIGNED_CERT');
+      done();
+    }).end();
+  });
+
+  after(function stopProxy(done) {
+    proxy.on('close', done);
+    proxy.close();
+  });
+});
